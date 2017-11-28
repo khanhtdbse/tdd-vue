721981b7cd08ad0a327ce8c2bfff052936366e8f
Tran Duy Khanh <tran.duy.khanh@framgia.com>
2017-11-28 17:56:30 +0700

    Add comment

--- test/unit/specs/ButtonComponent.spec.js
+++ test/unit/specs/ButtonComponent.spec.js
@@ -5,11 +5,13 @@ import ButtonComponent from '@/components/ButtonComponent'
 describe('ButtonComponent.vue', () => {
     let vm
 
+    // Khoi tao bien moi khi chay test case
     beforeEach(() => {
         const Constructor = Vue.extend(ButtonComponent)
         vm = new Constructor().$mount()
     })
 
+    // Kiem tra text o moi button
     it('should have to correct button text', () => {
         expect(vm.$data.text.increment_text)
         .to.equal('Click to increment counter')
@@ -17,15 +19,19 @@ describe('ButtonComponent.vue', () => {
         .to.equal('Click to decrement counter')
     })
 
+    // Kiem tra emit event khi click vao button
     it('should emit event when click button', () => {
+        // Tao spy cho vm.$emit
         sinon.spy(vm, '$emit')
 
-//        vm.onIncrementButtonClick()
+        // Click button increment
         vm.$refs.incrementButton.click()
+        // Assertion
         expect(vm.$emit).to.have.been.calledWith('incrementButtonHasBeenClicked')
-
-//       vm.onDecrementButtonClick()
+        
+        // Click button decrement
         vm.$refs.decrementButton.click()
+        // Assertion
         expect(vm.$emit).to.have.been.calledWith('decrementButtonHasBeenClicked')
     })
 
--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -5,39 +5,47 @@ import ButtonComponent from '@/components/ButtonComponent'
 describe('HelloWorld.vue', () => {
     let vm
 
+    // Khoi tao bien moi khi chay test case
     beforeEach(() => {
         const Constructor = Vue.extend(HelloWorld)
         vm = new Constructor().$mount()
     })
 
+    // Kiem tra noi dung the h1
     it('should render correct contents', () => {
         expect(vm.$el.querySelector('h1').textContent)
         .to.equal('Welcome to my first TDD :)')
     })
 
+    // Kiem tra bien count = 0
     it('should has correct init counter variable', () => {
         expect(vm.$data.count).to.equal(0)
     })
 
+    // Kiem tra HelloWorld component co child component
     it('should has child ButtonComponent', () => {
         expect(vm.$options.components.ButtonComponent).to.contain(ButtonComponent)
     })
 
+    // Kiem tra ham increment
     it('should increment count', () => {
         vm.increment()
         expect(vm.$data.count).to.equal(1)
     })
 
+    // Kiem tra ham decrement
     it('should decrement count', () => {
         vm.decrement()
         expect(vm.$data.count).to.equal(-1)
     })
 
+    // Kiem tra khi click vao button increment
     it('should increment count when click increment button', () => {
         vm.$el.querySelector('.increment-button').click()
         expect(vm.$data.count).to.equal(1)
     })
 
+    // Kiem tra khi click vao button decrement
     it('should decrement count when click decrement button', () => {
         vm.$el.querySelector('.decrement-button').click()
         expect(vm.$data.count).to.equal(-1)
